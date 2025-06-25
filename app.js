67ff6b7f9c2e6f88e59fb2b8f0804683133f9bde
AdrianC <car.vaz.montero@gmail.com>
2025-06-25 13:33:26 -0600

    Posible FInal

--- app.js
+++ app.js
@@ -3,19 +3,19 @@ const path = require('path');
 const indexRouter = require('./routes/index');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-// Serve static files from the "public" directory
+// Sirve archivos estáticos como CSS, imágenes, y .zip
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Use the router for handling routes
+// Usa tu router principal
 app.use('/', indexRouter);
 
-// Catch-all route for handling 404 errors
-app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-  });
+// Página 404 si no se encuentra una ruta
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
 
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
